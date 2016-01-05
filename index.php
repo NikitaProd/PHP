ef10f1f36b4793cf3445d70a71a78f7bbe48cae5
Simplon Nikita <nik.europe@gmail.com>
2016-01-05 17:23:16 +0100

    PHP TETST

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <h1>Articles</h1>
+    <?php
+    $articles=
+    [
+    ["Titre 1"," Popo"],
+    ["Titre 2","Toto"],
+    ["Titre 3"," Popo3"],
+    ["Titre 4"," Popo4"],
+    ["Titre 5"," Popo5"],
+    ];
+
+    for($i=0;$i<count($articles);$i++){
+    echo "<a href='article.php?article=$i'>". $articles[$i][0]."</a><br>";
+    }
+    ?>
+  </body>
+</html>
