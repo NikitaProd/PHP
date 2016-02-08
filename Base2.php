a0d6070434c47574abfa97b08b1bbcf4e91b7980
Nikita <nik.europe@gmail.com>
2016-02-08 16:16:05 +0100

    3

--- Base2.php
+++ Base2.php
@@ -70,3 +70,84 @@ echo $pseudo[3];
 
 <br />
 <br />
+
+<!-- #6 Les arrays  Profils-->
+
+<?php
+
+$profil = array('prenom' => 'Nik', 'pseudo' => 'Nik2');
+
+foreach($profil as $cle => $element)
+{
+  echo  'le cle '.$cle. ' c est '  .$element.'<br />';
+}
+?>
+
+<br />
+<br />
+
+
+<!-- #7 Les formulaires & données -->
+
+<?php
+
+echo $_GET['nombre'].'<br />';
+echo $_GET['chifre'];
+
+?>
+<!-- Inserrer pour afichage de chifres :http://localhost/index.php?nombre=28&chifre=30 -->
+<br />
+<br />
+
+<a href="?prenom=Nik&pseudo=Nik2">Dire Bonjour !</a>
+
+<?php
+if(isset($_GET['prenom']) AND isset($_GET['pseudo']))
+{
+  echo 'Lesse moi deviner ! Tu t\'appelles '.$_GET['prenom'].' et ton pseudo est '.$_GET['pseudo'].' !';
+}
+?>
+
+<br />
+<br />
+<br />
+
+<!-- #7 Les formulaires -->
+
+<?php
+if (isset($_POST['pseudo'])) {
+
+echo ' Ton pseudo est '. $_POST['pseudo'];
+}
+?>
+
+<form action="" method="post">
+<input type="text" name="pseudo" value="<?php if(isset($_POST['pseudo'])) { echo $_POST['pseudo']; } else { echo 'ton pseudo ici';}?>"/>
+<input type="submit" name="envoyer" value="Fait peter !"/>
+</form>
+
+<br />
+<br />
+<br />
+
+
+<!-- #7 Les formulaires Les chiffres -->
+
+<?php
+if (isset($_POST['ch'])) {
+$chiffre_securise = intval($_POST['ch']);
+$chiffre = 48;
+echo $chiffre. ' + '. $chiffre_securise. ' = '.$chiffre+$chiffre_securise;
+}
+?>
+<form action="" method="post">
+<input type="text" name="ch" value="<?php if(isset($_POST['ch'])) { echo $_POST['ch']; } else { echo 'ton chiffre ici';}?>"/>
+<input type="submit" name="envoyer" value="Ajouter 48 ! "/>
+</form>
+
+<br />
+<br />
+<br />
+
+
+
