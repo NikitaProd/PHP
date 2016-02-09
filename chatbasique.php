f423986d5e9a96cdd616baaae44c4a1aa2eef288
Nikita <nik.europe@gmail.com>
2016-02-09 16:41:51 +0100

    Update chatbasique.php

--- chatbasique.php
+++ chatbasique.php
@@ -1,10 +1,14 @@
 
-<h1>Chat Basique PHP</h1>
-
 <?php
 mysql_connect('localhost', 'root', 'root'); // connection a BD
 mysql_select_db('chat');
-?
+?>
+<h1>Chat basique</h1>
+<br />
+<hr />
+<br />
+
+
 
 <?php
 
