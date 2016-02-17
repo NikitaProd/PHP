197284078e533bcd584f6d814e54be151bf4d0bf
Nikita <nik.europe@gmail.com>
2016-02-17 16:47:44 +0100

    EX

--- crypt.php
+++ crypt.php
@@ -1,10 +1,22 @@
 
 crypter PHP  
 
-par ex : 
+
 
 <?php
 $var = 'Bonjour';
 $var_cryptee = md5($var);
 echo $var_cryptee;
 ?>
+
+par ex :  pour les mots de passe :
+
+EX:
+<?php
+  $pseudo = mysql_escape_string($_POST['pseudo']);
+  $motdepasse = mysql_escape_string (md5($_POST['motdepasse']));
+  $motdepasse2 = mysql_escape_string (md5($_POST['motdepasse2']));
+  $email = mysql_escape_string($_POST['email']);
+?>
+
+  
