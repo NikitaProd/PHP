eb7b4cbff596223759c0bce21244f2f4d278b028
Nikita <nik.europe@gmail.com>
2016-02-17 16:49:38 +0100

    Update crypt.php

--- crypt.php
+++ crypt.php
@@ -12,11 +12,26 @@ echo $var_cryptee;
 par ex :  pour les mots de passe :
 
 EX:
+
+register.php
 <?php
   $pseudo = mysql_escape_string($_POST['pseudo']);
   $motdepasse = mysql_escape_string (md5($_POST['motdepasse']));
   $motdepasse2 = mysql_escape_string (md5($_POST['motdepasse2']));
   $email = mysql_escape_string($_POST['email']);
 ?>
+
+
+login.php
+<?php
+/ Tout les champs ont ete remplis
+  $pseudo = mysql_escape_string($_POST['pseudo']);
+  $motdepasse = mysql_escape_string(md5($_POST['motdepasse']));
+?>
+
+
+
+
+
 
   
