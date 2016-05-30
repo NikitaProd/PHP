83f8619ee1161c8391d01469aff883317ae2981a
Nikita <nik.europe@gmail.com>
2016-05-30 09:56:42 +0200

    Update variables.php

--- variables.php
+++ variables.php
@@ -1,5 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
    "http://www.w3.org/TR/html4/strict.dtd">
+   
 <HTML>
    <HEAD>
    <TITLE>PHP VARIABLES</TITLE>
@@ -49,6 +50,7 @@
       ?>
 
      <!--  // Calcul : -->
+     
      <?php
      
      echo "<br />";
